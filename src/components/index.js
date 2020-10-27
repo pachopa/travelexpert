60ae4f55a64656aea05a788a630dd37f46931f88
Chris Lee <chrislee910320@gmail.com>
2020-10-27 13:44:34 -0600

    in the middle of creating a travelItem component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Navbar } from "./navigationBar";
 export { default as Carousel } from "./carousel";
 export { default as Footer } from "./footer";
+export { default as TravelItems } from "./travelItems";
