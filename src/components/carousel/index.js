0cfa71bfa20a46b6e1f68b09cc999fcc269f1a7a
Michael Grier <michael.c.grier@gmail.com>
2020-10-28 08:59:33 -0600

    style header, footer and carousel components

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -24,7 +24,7 @@ export default function ControlledCarousel() {
         <img className="d-block w-100" src={carribeanImage} alt="Carribean cruise" />
         <Carousel.Caption>
           <h3>Cruise the Carribean</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <p>Enjoy a relaxing cruise to a beautiful carribean island.</p>
         </Carousel.Caption>
       </Carousel.Item>
 
@@ -32,7 +32,7 @@ export default function ControlledCarousel() {
         <img className="d-block w-100" src={hawaiiImage} alt="Hawaiian resort" />
         <Carousel.Caption>
           <h3>Relax at a Hawaiian Resort</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p>Lounge on the beach at an exclusive Hawaiian resort.</p>
         </Carousel.Caption>
       </Carousel.Item>
 
@@ -40,19 +40,15 @@ export default function ControlledCarousel() {
         <img className="d-block w-100" src={japanImage} alt="Eco-tour of Asia" />
         <Carousel.Caption>
           <h3>Experience the Natural Beauty of Asia</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          <p>Explore the unique flora and fauna of China, Japan and Korea.</p>
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item className={classes.item}>
         <img className="d-block w-100" src={greeceImage} alt="Train tour of Europe" />
         <Carousel.Caption>
-          <h3>Ride the Rails in Through Europe</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          <h3>Ride the Rails Through Europe</h3>
+          <p>Traverse the vast and diverse continent of Europe on their first-class rail system.</p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
--- src/components/carousel/styles.js
+++ src/components/carousel/styles.js
@@ -2,6 +2,7 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles({
   item: {
-    height: 600,
-  },
+    height: 555,
+    objectFit: "cover"
+  }
 });
